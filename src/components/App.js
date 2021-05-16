e710c31e4c798d6582e09b6f2d71088ccc7f0844
Christopher Ritter <christopherritter@gmail.com>
2021-05-16 17:47:08 -0400

    created update profile component

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import PrivateRoute from "./PrivateRoute"
+import UpdateProfile from "./UpdateProfile"
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <AuthProvider>
             <Switch>
               <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
             </Switch>
