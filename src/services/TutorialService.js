5c3645b87b752f3fcbf601fba0e8efdab273bbcb
Christopher Ritter <christopherritter@gmail.com>
2021-05-17 15:21:19 -0400

    implemented tutorial service

--- src/services/TutorialService.js
+++ src/services/TutorialService.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { firestore } from "../firebase";
 
-export default function TutorialService() {
-  return (
-    <div>
-      Tutorial Service
-    </div>
-  )
-}
+const db = firestore.collection("/tutorials");
+
+const getAll = () => {
+  return db;
+};
+
+const create = (data) => {
+  return db.add(data);
+};
+
+const update = (id, value) => {
+  return db.doc(id).update(value);
+};
+
+const remove = (id) => {
+  return db.doc(id).delete();
+};
+
+const TutorialService = {
+  getAll,
+  create,
+  update,
+  remove
+};
+
+export default TutorialService;
