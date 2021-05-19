75a12ec3e7668c90ec08f6567649edc55983b29a
Christopher Ritter <christopherritter@gmail.com>
2021-05-19 14:01:29 -0400

    created initial Quest states

--- src/components/quests/Quest.jsx
+++ src/components/quests/Quest.jsx
@@ -1,14 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import QuestDataService from "../../services/QuestService";
 
-const Quest = () => {
+const Quest = (props) => {
+  const initialQuestState = {
+    author: "",
+    authorId: "",
+    categories: [],
+    description: "",
+    featured: false,
+    image: "",
+    isAnonymous: false,
+    isFeatured: false,
+    questId: "",
+    startingPoint: "",
+    title: "",
+  };
+  const initialRegionState = {
+    coordinates: [],
+    name: "North Park",
+    zoom: 17,
+  };
+  
+  const [currentQuest, setCurrentQuest] = useState(initialQuestState);
+  const [currentRegion, setCurrentRegion] = useState(initialRegionState);
+  const [error, setError] = useState("");
+
+  const { quest } = props;
+
+  useEffect(() => {
+    const unsubscribe = QuestDataService.getAll()
+      .where("questId", "==", props.questId)
+      .onSnapshot((snapshot) => {
+        snapshot.docs.map((doc) => setCurrentQuest(doc.data()));
+      });
+
+    return unsubscribe;
+  }, [currentQuest]);
+
   return (
     <Container>
       <Row>
-        <Col><h1>Quest</h1></Col>
+        <Col>
+          <h1>Quest</h1>
+        </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Quest
+export default Quest;
