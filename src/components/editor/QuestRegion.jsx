f7155a3111b9fcae2cf7ad5d1d3c30fa8acfa95f
Christopher Ritter <christopherritter@gmail.com>
2021-05-22 12:55:24 -0400

    set coords to read only

--- src/components/editor/QuestRegion.jsx
+++ src/components/editor/QuestRegion.jsx
@@ -1,65 +1,69 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 
-const QuestRegion = props => {
-
+const QuestRegion = (props) => {
   const onUpdateCenter = (event) => {
     props.updateCenter(event.coordinates);
   };
 
-  const map = React.cloneElement(props.map, { updateCenter: onUpdateCenter });
+  const QuestMap = React.cloneElement(props.map, {
+    updateCenter: onUpdateCenter,
+  });
 
   return (
     <Grid container spacing={4}>
       <Grid item sm={3}>
-      <form noValidate>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="regionCoords"
-          label="Coordinates"
-          name="coordinates"
-          value={props.region.coordinates}
-          onChange={props.updateRegion}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="regionName"
-          label="Name"
-          name="name"
-          value={props.region.name}
-          onChange={props.updateRegion}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="regionZoom"
-          label="Zoom"
-          name="zoom"
-          value={props.region.zoom}
-          onChange={props.updateRegion}
-        />
-      </form>
+        <form noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="regionCoords"
+            label="Coordinates"
+            name="coordinates"
+            value={props.region.coordinates}
+            onChange={props.updateRegion}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="regionName"
+            label="Name"
+            name="name"
+            value={props.region.name}
+            onChange={props.updateRegion}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="regionZoom"
+            label="Zoom"
+            name="zoom"
+            value={props.region.zoom}
+            onChange={props.updateRegion}
+          />
+        </form>
       </Grid>
       <Grid item sm={9}>
-        { map }
+        {QuestMap}
       </Grid>
     </Grid>
   );
-}
+};
 
 QuestRegion.propTypes = {
   region: PropTypes.object,
-}
+};
 
-export default QuestRegion
+export default QuestRegion;
