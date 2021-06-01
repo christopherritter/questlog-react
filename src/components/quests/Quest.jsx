81767f2a67c7dabf41e8d8fb8ad7a2e9933d172c
Christopher Ritter <christopherritter@gmail.com>
2021-06-01 12:02:31 -0400

    updated layout

--- src/components/quests/Quest.jsx
+++ src/components/quests/Quest.jsx
@@ -1,8 +1,38 @@
 import React, { useState, useEffect } from "react";
-import Grid from "@material-ui/core/Grid";
 import QuestDataService from "../../services/QuestService";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from '@material-ui/core/Typography';
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  headerTitle: {
+    marginTop: "1.5em",
+  },
+  headerText: {
+    marginBottom: "1.5em",
+    marginBottom: "2em",
+  },
+  headerButtonBar: {
+    marginBottom: "1em",
+    marginBottom: "4em",
+  },
+  headerButtons: {
+    marginRight: "1em",
+  }
+}));
+
 const Quest = (props) => {
+  const classes = useStyles();
+
   const initialQuestState = {
     author: "",
     authorId: "",
@@ -22,13 +52,13 @@ const Quest = (props) => {
     name: "Geographic Center of the United States",
     zoom: 12.5,
   };
-  
+
   const [quest, setQuest] = useState(initialQuestState);
   const [currentRegion, setCurrentRegion] = useState(initialRegionState);
   const [error, setError] = useState("");
 
   useEffect(() => {
-    console.log(props)
+    console.log(props);
     const unsubscribe = QuestDataService.getAll()
       .where("questId", "==", props.match.params.questId)
       .onSnapshot((snapshot) => {
@@ -39,10 +69,24 @@ const Quest = (props) => {
   }, [props]);
 
   return (
-    <Grid>
-      <h1 className="mt-5">{ quest.title }</h1>
-      <p>{ quest.description }</p>
-    </Grid>
+    <Paper elevation={0} className={classes.root}>
+      <Container>
+        <Grid container>
+          <Grid sm={12} item>
+            <Typography variant="h2" gutterBottom className={ classes.headerTitle }>{quest.title}</Typography>
+            <Typography variant="subtitle1" className={ classes.headerText }>{quest.description}</Typography>
+            <Box className={classes.headerButtonBar}>
+              <Button variant="contained" className={classes.headerButtons} color="primary">Play</Button>
+              <Button variant="contained" className={classes.headerButtons}>Read</Button>
+              <Button variant="contained" className={classes.headerButtons}>Edit</Button>
+            </Box>
+          </Grid>
+          <Grid sm={12} item>
+
+          </Grid>
+        </Grid>
+      </Container>
+    </Paper>
   );
 };
 
