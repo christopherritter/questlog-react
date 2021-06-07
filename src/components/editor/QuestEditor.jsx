aa218b92578930a04b5770250e6b7eae917342f0
Christopher Ritter <christopherritter@gmail.com>
2021-06-07 11:06:07 -0400

    code clean-up

--- src/components/editor/QuestEditor.jsx
+++ src/components/editor/QuestEditor.jsx
@@ -20,15 +20,6 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Box from "@material-ui/core/Box";
 
-const layerStyle = {
-  id: "point",
-  type: "circle",
-  paint: {
-    "circle-radius": 10,
-    "circle-color": "#007cbf",
-  },
-};
-
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -68,76 +59,15 @@ function a11yProps(index) {
   };
 }
 
-const QuestEditor = (props) => {
+const QuestEditor = () => {
   const classes = useStyles();
-  const {
-    quest,
-    location,
-    entry,
-    item,
-    action,
-    locationIndex,
-    addLocation,
-    updateLocation,
-    removeLocation,
-    clearLocation,
-    addEntry,
-    updateEntry,
-    removeEntry,
-    clearEntry,
-    addItem,
-    updateItem,
-    removeItem,
-    clearItem,
-    addAction,
-    updateAction,
-    removeAction,
-    clearAction,
-    publishQuest,
-    findWithAttr,
-    setLocation,
-    setLocationIndex,
-    entryIndex,
-    actionIndex,
-    itemIndex,
-  } = useContext(QuestContext);
+  const { quest } = useContext(QuestContext);
 
   const viewTab = (event, newTab) => {
     setTab(newTab);
   };
 
   const [tab, setTab] = useState(0);
-
-  const onMapPointClick = (event) => {
-    const { id } = event.features[0].properties;
-    const index = findWithAttr(quest.locations, "id", id);
-    setLocationIndex(index);
-    setLocation(quest.locations[index]);
-  };
-
-  const geojson = {
-    type: "FeatureCollection",
-    features: quest.locations
-      ? quest.locations.map((location) => {
-          var feature = {
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [location.longitude, location.latitude],
-            },
-            properties: {
-              id: location.id,
-              name: location.name,
-              bearing: location.bearing,
-              pitch: location.pitch,
-              zoom: location.zoom,
-              marker: location.marker,
-            },
-          };
-          return feature;
-        })
-      : [],
-  };
 
   return (
     <Paper elevation={0} className={classes.root}>
@@ -176,35 +106,7 @@ const QuestEditor = (props) => {
             <QuestEntries />
           </TabPanel>
           <TabPanel value={tab} index={5}>
-            <QuestItems
-              map={
-                <MapGL
-                  style={{ width: "100%", height: "400px" }}
-                  mapStyle="mapbox://styles/mapbox/streets-v11"
-                  accessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-                >
-                  <Source id="locationsData" type="geojson" data={geojson} />
-                  <Layer
-                    source="locationsData"
-                    onClick={onMapPointClick}
-                    {...layerStyle}
-                  />
-                </MapGL>
-              }
-              region={quest.region}
-              objectives={quest.objectives}
-              locations={quest.locations}
-              locationIndex={locationIndex}
-              location={location}
-              items={quest.items}
-              itemIndex={itemIndex}
-              item={item}
-              addItem={addItem}
-              updateItem={updateItem}
-              removeItem={removeItem}
-              clearItem={clearItem}
-              publishQuest={publishQuest}
-            ></QuestItems>
+            <QuestItems />
           </TabPanel>
         </Grid>
       </Grid>
