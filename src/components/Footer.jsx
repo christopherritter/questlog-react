4b3e222d9fd87a5a38eecbac40aab80986734f20
Christopher Ritter <christopherritter@gmail.com>
2021-06-12 13:38:44 -0400

    adjusted Home components layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,18 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     backgroundColor: theme.palette.background.footer,
+    marginTop: theme.spacing(12)
   },
   copy: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  }
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
 }));
 
 const Footer = () => {
@@ -22,8 +24,11 @@ const Footer = () => {
   return (
     <Paper square elevation="0" className={classes.root}>
       <Container>
-        <Typography variant="body1" color="inherit" className={classes.copy}>
-          © {new Date().getFullYear()} Christopher Ritter
+        <Typography variant="body2" color="textSecondary" align="center" className={classes.copy}>
+          {"Copyright © " + new Date().getFullYear() + " " }
+          <Link color="inherit" href="http://www.christopherritter.com/">
+            Christopher Ritter
+          </Link>
         </Typography>
       </Container>
     </Paper>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,15 +12,87 @@ import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Link from "@material-ui/core/Link";
+import Button from "@material-ui/core/Button";
+
+import CompassOutlineIcon from "mdi-material-ui/CompassOutline";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
-    marginBottom: theme.spacing(10),
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  jumboContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+    marginBottom: theme.spacing(6)
+  },
+  jumboIcon: {
+    fontSize: 60,
+    marginRight: theme.spacing(1),
+    marginBottom: theme.spacing(-1),
+  },
+  jumboButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
   },
 }));
+
+const Jumbotron = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.jumboContent}>
+      <Container maxWidth="sm">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+          className={classes.jumboTitle}
+        >
+          {/* <CompassOutlineIcon className={classes.jumboIcon} /> */}
+          QuestLog
+        </Typography>
+        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+          A storytelling platform for playing text-based adventures on an
+          interactive map.
+        </Typography>
+        <div className={classes.jumboButtons}>
+          <Grid container spacing={2} justify="center">
+            <Grid item>
+              <Button variant="contained" color="primary">
+                Main call to action
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" color="primary">
+                Secondary action
+              </Button>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 const Home = () => {
   const classes = useStyles();
@@ -30,18 +102,16 @@ const Home = () => {
 
   return (
     <>
-      <Container className={classes.root}>
-        <Grid container>
-          <Grid item xs={12}>
-            <h1>Home</h1>
-          </Grid>
-          <Grid item xs={12}>
-            {error && <strong>Error: {error}</strong>}
-            {loading && <span>Loading...</span>}
-            {!loading &&
-              quests &&
-              quests.docs.map((quest, index) => (
-                <Card style={{ width: "18rem" }} key={index}>
+      <Container className={classes.cardGrid} maxWidth="md">
+        <Jumbotron />
+        <Grid container spacing={4}>
+          {error && <strong>Error: {error}</strong>}
+          {loading && <span>Loading...</span>}
+          {!loading &&
+            quests &&
+            quests.docs.map((quest, index) => (
+              <Grid item key={index} xs={12} sm={6} md={4}>
+                <Card className={classes.card} key={index}>
                   <CardContent>
                     <Typography variant="h5" component="h2">
                       {quest.data().title}
@@ -50,18 +120,19 @@ const Home = () => {
                       {quest.data().description}
                     </Typography>
                     <CardActions>
-                      <Link
+                      <Button
+                        color="primary"
                         component={RouterLink}
                         to={`/quest/${quest.data().questId}`}
                         className="btn btn-primary"
                       >
                         View Quest
-                      </Link>
+                      </Button>
                     </CardActions>
                   </CardContent>
                 </Card>
-              ))}
-          </Grid>
+              </Grid>
+            ))}
         </Grid>
       </Container>
       <Footer />
