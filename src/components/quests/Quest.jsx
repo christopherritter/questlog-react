718d11699ada1f61f21b258c454122e3882b1efc
Christopher Ritter <christopherritter@gmail.com>
2021-07-16 14:41:51 -0400

    code clean-up

--- src/components/quests/Quest.jsx
+++ src/components/quests/Quest.jsx
@@ -357,8 +357,6 @@ const Quest = (props) => {
   // Actions may be taken by the user to move the story
   // They are associated with Entries and Items
 
-  // const [actionIndex, setActionIndex] = useState(-1);
-
   const [action, setAction] = useState();
 
   function handleAddAction(action) {
@@ -386,12 +384,10 @@ const Quest = (props) => {
     console.log("Quest: handleRemoveAction", action.id);
     const updatedActions = quest.actions.filter((a) => a.id !== action.id);
     setQuest({ ...quest, actions: updatedActions });
-    // setActionIndex(-1);
     setAction(null);
   }
 
   function handleClearAction() {
-    // setActionIndex(-1);
     setAction(null);
   }
 
